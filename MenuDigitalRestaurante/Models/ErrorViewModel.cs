dac2d429967d10cd20263f68e3173aa06b0b6bad
FranciscoCanoLopez <I23050331@monclova.tecnm.mx>
2026-09-13 21:38:07 -0600

    Agregar archivos de proyecto.

---
+++ MenuDigitalRestaurante/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MenuDigitalRestaurante.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
