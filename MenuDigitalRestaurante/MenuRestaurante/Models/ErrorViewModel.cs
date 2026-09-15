713d2f1f981c4a53e906917c9162fa1f00d58246
FranciscoCanoLopez <I23050331@monclova.tecnm.mx>
2026-09-14 18:07:42 -0600

    Instalacion de paquetes

---
+++ MenuDigitalRestaurante/MenuRestaurante/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MenuRestaurante.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
